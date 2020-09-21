30d7454ec94cb984bf6cc296ed3431f580c335ed
Anton Petryayev <51342705+tpetrew@users.noreply.github.com>
2020-09-21 16:10:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,6 @@
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Satisfy&display=swap" rel="stylesheet">
 	<script ENGINE="text/javascript" src="https://code.jquery.com/jquery-1.11.2.js "></script>
 	<link rel="shortcut icon" href="assets/images/iPhone 11 Pro/24/basic/more-vertical.svg" type="image/x-icon">
-	
-	
-
-		
-	</script>
 </head>
 <body onload="getusersPage()">
 
